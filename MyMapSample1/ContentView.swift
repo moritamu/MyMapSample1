7f509fff0c0eccc921b638ead3b51acb128987c5
moritamu <moris@r.sannet.ne.jp>
2021-11-28 09:15:37 +0900

    Initial Commit

---
+++ MyMapSample1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyMapSample1
+//
+//  Created by 森田晋 on 2021/11/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyMapSample1/MyMapSample1App.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapSample1App.swift
+//  MyMapSample1
+//
+//  Created by 森田晋 on 2021/11/28.
+//
+
+import SwiftUI
+
+@main
+struct MyMapSample1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
