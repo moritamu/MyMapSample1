2db36852bed491e5b7fac59be6ab50163d63bc62
moritamu <moris@r.sannet.ne.jp>
2021-11-28 17:46:32 +0900

    last1

--- MyMapSample1/MapView.swift
+++ MyMapSample1/MapView.swift
@@ -11,12 +11,14 @@ import MapKit
 struct MapView: UIViewRepresentable {
 //    UIViewRepresentableにはmakeUIViewとupdateUIViewが必要
     let searchKey:String//検索キー
+    let mapType: MKMapType
     
     func makeUIView(context: Context) -> MKMapView {
         MKMapView()
     }
     func updateUIView(_ uiView: MKMapView, context: Context) {
         print(searchKey)
+        uiView.mapType = mapType
         let geocoder = CLGeocoder()
         geocoder.geocodeAddressString(searchKey, completionHandler: { (placemarks,error) in
             if let unwrapPlacemarks = placemarks ,
@@ -41,6 +43,6 @@ struct MapView: UIViewRepresentable {
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView(searchKey: "東京タワー")
+        MapView(searchKey: "東京タワー",mapType: .standard)
     }
 }
